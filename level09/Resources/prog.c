eb701224839b0ee036cb32e313989c2dd298fce9
hstiv <satmak335@gmail.com>
2021-08-31 21:17:12 +0300

    doen

---
+++ level09/Resources/prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char **argv)
+{
+        int i=0;
+        while (argv[1][i] != '\0')
+        {
+                printf("%c", argv[1][i]-i);
+                i++;
+        }
+        printf("\n");
+        return (0);
+}
